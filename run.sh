9ecfdb1005d3f1f73438fa4cdd753ac9ce4452dc
mickrick1 <60800454+mickrick1@users.noreply.github.com>
2021-09-02 15:57:46 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./src/main.py --display pygame --width 256 --height 64 --interface i2c --mode 1 --rotate 2
+python ./src/main.py --display pygame --width 256 --height 64 --interface i2c 
